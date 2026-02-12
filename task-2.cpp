1c823edb9e46b89673e54d2f1f1def1067e05a8b
vladislava-iva <vladislava.ivanova109@gmail.com>
2026-02-12 12:18:19 +0300

    Update task-2.cpp

--- task-2.cpp
+++ task-2.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 using namespace std;
@@ -33,9 +34,41 @@ double func(double x, int &k)
 }
 
 
-int main()
-{
+int main() {
+    const double e = 3e-8;
+    const double e2 = e / 2.0;
+
+    cout << fixed << setprecision(2);
+
+    double sq = 2.0 / e2;
+    double n = sq * sq;
+
+    long long n1 = ceil(n);
+    long long n2 = 2LL * n1;
+
+    cout << "n на 1 ряд ≥ " << n1 << "\n";
+    cout << "всего членов: " << n2 << "\n";
+
+    const double t_m = 500.0;
+    const double t_s  = t_m * 1e-6;
+
+    double t1 = n1 * t_s;
+    double t2 = n2 * t_s;
+
+    const double t_y = 365 * 24 * 3600.0;
+
+    double y1 = t1 / t_y;
+    double y2 = t2 / t_y;
+
+    cout << "время на одно слагаемое: " << t_m << " мкс"<<endl;
+
+    cout << "время на один ряд: " << t1 << " c или "<< (long long)y1 << " лет"<<endl;
+
+    cout << "время на оба ряда: "<< t2 << " c или " << (long long)y2 << " лет"<<endl;
+
     double xs[] = {0.5, 0.999999999};
+
+    cout << fixed << setprecision(8);
 
     for (double x : xs)
     {
